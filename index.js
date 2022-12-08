e5926d54a99b7d6fe14a2301ce536d35b75576f5
Diego <diego.moreira529@gmail.com>
2022-12-08 13:45:03 -0300

    Render city name and current temp

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 async function getWeather(cityName) {
-  const response = await fetch(
+  let response = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=f665e17e864ad3ebcceb327ae8c2131a`,
     { mode: "cors" }
   );
-
-  const cityWeather = await response.json();
-
-  console.log(cityWeather);
+  response = await response.json();
+  return response;
 }
 
 const searchBar = document.querySelector("#city-search-bar");
 searchBar.addEventListener("submit", submitSearch);
+
 function submitSearch(e) {
+  // Calls API with the input and render content
   e.preventDefault();
   const input = e.target[0].value;
-  getWeather(input);
+  getWeather(input).then((response) => renderWeather(response));
+}
+
+function renderWeather(weatherObj) {
+  console.log(weatherObj);
+
+  const weatherContainer = document.querySelector("#weather-container");
+  const cityName = weatherContainer.querySelector(".city-name");
+  const currentTemperature = weatherContainer.querySelector(
+    ".current-temperature"
+  );
+
+  cityName.textContent = weatherObj.name;
+  currentTemperature.textContent = weatherObj.main.temp;
 }
